385fa42161d61b826f9ae7c3edd6c9ba2b5972af
Sarah Flynn <sarah.flynn@icloud.com>
2018-07-27 15:28:52 -0700

    made code more readable, reorganized elements

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var word;
 var correctLetters = 0;
 var incorrectLetters = 0;
 
+var wordArray;
+var letterGuess;
+
 function startGame(){
     randomWordCreator();
     document.getElementById('game-area').style.visibility = 'visible';
@@ -16,36 +19,61 @@ function randomWordCreator(){
     var arrayLength = wordList.length;
     var randomIndex = Math.floor(Math.random() * arrayLength);
     word = wordList[randomIndex];
+    wordArray = word.split('');
+    wordSpaceCreator();
+    console.log(word);   
+}
 
-    console.log(word);
-
-    return false;
+function wordSpaceCreator() {
+    var wordSpace = '';
+    for(var i = 0; i < word.length; i++) {
+        wordSpace += `<td id="letter-${i}"></td>`;
+    }
+    document.getElementById('word-space').innerHTML = wordSpace;
 }
 
 function submitGuess() {
     console.log('submit guess working');
     console.log('game word', word);
+    
+    letterGuess = document.getElementById('guess').value.toLowerCase();
+    
+    correctBox();
+    
+    incorrectBox();
+    
+    winLoss();
+    
+    return false;
+}
 
-    var wordArray = word.split('');
-    var letterGuess = document.getElementById('guess').value.toLowerCase();
 
+
+
+function correctBox() {
     for(var i = 0; i < word.length; i++){
-
+        
         if(letterGuess === wordArray[i]){
             document.getElementById('letter-' + i).innerText = letterGuess;
             correctLetters ++;
         }
     }
+}
 
+function incorrectBox() {
     if(wordArray.indexOf(letterGuess) === -1) {
         document.getElementById('guess-' + incorrectLetters).innerText = letterGuess;
-
-        var imageNumber = incorrectLetters + 4;
-        document.getElementById('gallows').innerHTML = '<img src="https://www.oligalma.com/downloads/images/hangman/files/' + imageNumber + '.jpg">';
-        
+        gallows();
         incorrectLetters ++;
     }
+}
 
+function gallows() {
+    var imageNumber = incorrectLetters + 4;
+    document.getElementById('gallows').innerHTML = '<img src="https://www.oligalma.com/downloads/images/hangman/files/' + imageNumber + '.jpg">';
+}
+function winLoss() {
+    
     if(correctLetters === wordArray.length){
         document.getElementById('results').innerText = 'You won!';
     }
@@ -53,11 +81,4 @@ function submitGuess() {
     if(incorrectLetters > 6){
         document.getElementById('results').innerText = 'You lost!';
     }
-
-
-    return false;
-}
-
-function gallows(){
-    
 }
