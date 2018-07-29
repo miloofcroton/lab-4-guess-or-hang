6bda34935fecfe0adf83b365ec20d363923cf816
Jack <ajackofsometrades@gmail.com>
2018-07-28 22:31:48 -0700

    cleaned up script...procrastinating

--- word-script.js
+++ word-script.js
@@ -1,10 +1,18 @@
 /* eslint-disable */
 
-// I went to http://www.simpsoncrazy.com/dictionary and ran this in the console
+// I went to http://www.simpsoncrazy.com/dictionary and ran this in the console.
+// I figured we could add the capacity to display non-letter characters, but it was easier to remove them for now.
+// Plus, Hangman users wouldn't expect to be guessing a word that has special characters or spaces. That's Jeopardy, not Hangman.
 
-wordElements = document.getElementsByTagName('h3');
+var wordElements;
 var wordArray = [];
 var wordString = "";
+
+function initiate(){
+    wordElements = document.getElementsByTagName('h3');
+    var wordArray = [];
+    var wordString = "";
+}
 
 function elementToArray(){
     for (var i = 0; i < wordElements.length; i++) {
@@ -12,11 +20,24 @@ function elementToArray(){
     }
 }
 
-function cleanArray(){
+function onlyVocab(){
+    var newArray = [];
     for (var i = 0; i < 9; i++) {
         wordArray.shift();
     }
+}
 
+function onlyLetters(){
+    for (var i = 0; i < wordArray.length; i++) {
+        var onlyLetters = new RegExp('[a-z, A-Z]');
+        newArray.push(onlyLetters.exec(wordArray[i]))
+    }
+    wordArray = newArray;
+}
+
+function cleanArray(){
+    onlyVocab();
+    onlyLetters();
 }
 
 function arrayToString(){
@@ -26,6 +47,7 @@ function arrayToString(){
 }
 
 function returnWords() {
+    initiate();
     elementToArray();
     cleanArray();
     arrayToString();
