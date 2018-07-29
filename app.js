0e7f3c4af2d1303dac97b267209635b189e60f72
Jack <ajackofsometrades@gmail.com>
2018-07-29 12:41:48 -0700

    combined state functions into case statements

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-/* exported startGame, submitGuess, resetGame */
+/* exported startGame, submitGuess */
 /* globals wordList */
+
+
+
+var gameState = {
+    word: {
+        asString: '',
+        asCharArray: [],
+    },
+    guesses: {
+        number: {
+            incorrectGuesses: 0,
+            correctGuesses: 0,
+            totalGuesses: 0,
+            remainingGuesses: 0,
+        },
+        letters: [],
+    },
+    game: {
+        intial: true,
+        midGame: false,
+        victory: false,
+        loss: false,
+    }
+};
 
 
 
@@ -17,11 +41,66 @@ var submitButton = document.getElementById('submit-button');
 var resetButton = document.getElementById('reset-button');
 
 
-function startGame(){
-    randomWordCreator();
-    document.getElementById('game-area').style.visibility = 'visible';
-    triesLeft();
-    startAreaHidden();
+function startGame(action){
+    switch(action){
+        case 'start':
+            randomWordCreator();
+            document.getElementById('game-area').style.visibility = 'visible';
+            triesLeft();
+            startArea('hide');
+            break;
+        case 'reset':
+            for(var i = 0; i <= incorrectLetters; i++) {
+                document.getElementById('guess-' + i).innerText = '';
+            }
+
+            document.getElementById('gallows').innerHTML = '<img src="/assets/0.jpg">';
+            document.getElementById('results').innerText = '';
+    
+            word = '';
+            correctLetters = 0;
+            incorrectLetters = 0;
+            wordArray = [];
+            lettersGuessed = [];
+    
+            buttonControl('enable');
+            document.getElementById('game-area').style.visibility = 'hidden';
+    
+            startArea('show');
+    
+            console.log(`on reset: word is ${word}, correctLetters is ${correctLetters}, incorrectLetters is ${incorrectLetters}, wordArray is ${wordArray}, lettersGuessed is ${lettersGuessed}`);
+            
+            break;
+
+        default:
+            console.log('startGame must have an action argument');
+    }
+}
+
+function startArea(action){
+    switch(action){
+        case 'hide':
+            document.getElementById('start-area').style.visibility = 'hidden';
+            break;
+        case 'show':
+            document.getElementById('start-area').style.visibility = 'visible';
+            break;
+        default:
+            console.log('startArea must have an action argument');
+    }
+}
+function buttonControl(action){
+    switch(action) {
+        case 'enable':
+            submitButton.disabled = false;
+            break;
+        case 'disable':
+            submitButton.disabled = true;
+            resetButton.disabled = false;            
+            break;
+        default:
+            console.log('buttonControl must have an action argument');
+    }
 }
 
 function randomWordCreator(){
@@ -45,7 +124,7 @@ function submitGuess() {
     var letterGuess = document.getElementById('guess').value.toLowerCase();
     console.log('Their guess was: ', letterGuess);
 
-    document.getElementById("guess").value = "";
+    document.getElementById('guess').value = '';
 
     if(lettersGuessed.includes(letterGuess)) {
         alert('You have already guessed that letter. Please try again.');
@@ -97,63 +176,15 @@ function gallows() {
 function winLoss() {
     
     if(correctLetters === wordArray.length){
-
         document.getElementById('results').innerText = 'You won!';
         document.getElementById('guesses-left').innerText = '';
-        submitButtonDisable();
+        buttonControl('disable');
     }
     
     if(incorrectLetters > 6){
         document.getElementById('results').innerText = 'You lost!';
         document.getElementById('guesses-left').innerText = '';
-        submitButtonDisable();
+        buttonControl('disable');
     }
 
-}
-
-function startAreaHidden() {
-    document.getElementById('start-area').style.visibility = 'hidden';
-}
-
-function startAreaVisible() {
-    document.getElementById('start-area').style.visibility = 'visible';
-}
-
-function submitButtonEnable() {
-    submitButton.disabled = false;
-}
-
-function submitButtonDisable() {
-    submitButton.disabled = true;
-    resetButton.disabled = false;
-}
-
-
-
-function resetGame() {
-
-    console.log('reset working');
-
-    for(var i = 0; i <= incorrectLetters; i++) {
-        document.getElementById('guess-' + i).innerText = '';
-    }
-
-    document.getElementById('gallows').innerHTML = '<img src="https://www.oligalma.com/downloads/images/hangman/files/3.jpg">';
-
-    document.getElementById('results').innerText = '';
-
-    word = '';
-    correctLetters = 0;
-    incorrectLetters = 0;
-    wordArray = [];
-    lettersGuessed = [];
-
-    submitButtonEnable();
-    document.getElementById('game-area').style.visibility = 'hidden';
-
-    startAreaVisible();
-    
-    console.log(`on reset: word is ${word}, correctLetters is ${correctLetters}, incorrectLetters is ${incorrectLetters}, wordArray is ${wordArray}, lettersGuessed is ${lettersGuessed}`);
-
-    
 }
